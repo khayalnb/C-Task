6540253e3b8041f913ece7b67427082d59b65ee2
khayalnb <xeyal.binnetov@profit.az>
2021-09-22 13:42:58 +0400

    C# task

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, temp;
+            Console.WriteLine("1 ci eded");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("2 ci eded");
+            number2 = int.Parse(Console.ReadLine());
+            temp = number1;
+            number1 = number2;
+            number2=temp ;
+            Console.WriteLine("Sonra");
+            Console.WriteLine("1ci eded"+number1);
+            Console.WriteLine("2ci eded"+number2);
+
+
+        }
+    }
+}
