99e511d69a4bfa1482f5e07a4a1442e4b6d91b6a
khayalnb <xeyal.binnetov@profit.az>
2021-09-22 13:54:30 +0400

    C# Task2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,17 +6,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int number1, number2, temp;
-            Console.WriteLine("1 ci eded");
-            number1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("2 ci eded");
-            number2 = int.Parse(Console.ReadLine());
-            temp = number1;
-            number1 = number2;
-            number2=temp ;
-            Console.WriteLine("Sonra");
-            Console.WriteLine("1ci eded"+number1);
-            Console.WriteLine("2ci eded"+number2);
+            //int number1, number2, temp;
+            //Console.WriteLine("1 ci eded");
+            //number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("2 ci eded");
+            //number2 = int.Parse(Console.ReadLine());
+            //temp = number1;
+            //number1 = number2;
+            //number2=temp ;
+            //Console.WriteLine("Sonra");
+            //Console.WriteLine("1ci eded"+number1);
+            //Console.WriteLine("2ci eded"+number2);
+
+            Console.WriteLine("Birinci ededi daxil edin");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ucuncu ededi daxil edin");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1*num2*num3);
+           
+
+
+
 
 
         }
